54edbb5c59ce0acc0e6a2f04a7f42019646f93c0
Roman-Tech-Plus <rrcabrahamson@gmail.com>
2023-09-16 09:26:40 -0700

    ...

--- src/main/java/frc/robot/BreakerLib/devices/motors/smart/BreakerGenericSmartMotorController.java
+++ src/main/java/frc/robot/BreakerLib/devices/motors/smart/BreakerGenericSmartMotorController.java
@@ -1,54 +1,51 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.BreakerLib.devices.motors.smart;
+package frc.robot.BreakerLib.devices.motors.smart;
 
-// import edu.wpi.first.util.sendable.Sendable;
-// import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-// import frc.robot.BreakerLib.devices.encoders.BreakerGenericEncoder;
-// import frc.robot.BreakerLib.util.logging.advantagekit.BreakerLoggable;
-// import frc.robot.BreakerLib.util.logging.advantagekit.LogTable;
-// import frc.robot.BreakerLib.util.test.selftest.BreakerSelfTestable;
-// import frc.robot.BreakerLib.util.test.selftest.DeviceHealth;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import frc.robot.BreakerLib.devices.encoders.BreakerGenericEncoder;
+import frc.robot.BreakerLib.util.logging.advantagekit.BreakerLoggable;
+import frc.robot.BreakerLib.util.logging.advantagekit.LogTable;
+import frc.robot.BreakerLib.util.test.selftest.BreakerSelfTestable;
+import frc.robot.BreakerLib.util.test.selftest.DeviceHealth;
 
-// /** Add your docs here. */
-// public interface BreakerGenericSmartMotorController extends BreakerSelfTestable, BreakerLoggable, MotorController, Sendable, AutoCloseable {
+/** Add your docs here. */
+public interface BreakerGenericSmartMotorController extends BreakerSelfTestable, BreakerLoggable, MotorController, Sendable, AutoCloseable {
 
-//     public abstract double getDrawCurrent();
-//     public abstract void setBrakeMode();
-//     public default BreakerGenericEncoder getRotorEncoder() {
-//         return getEncoder(1);
-//     }
-//     public default BreakerGenericEncoder getSelectedEncoder() {
-//         return getEncoder(0);
-//     }
-//     public abstract BreakerGenericEncoder getEncoder(int encoderNum);
+    public abstract double getDrawCurrent();
+    public abstract void setBrakeMode();
+    public default BreakerGenericEncoder getRotorEncoder() {
+        return getEncoder(0);
+    }
+    public abstract BreakerGenericEncoder getSelectedEncoder();
+    public abstract BreakerGenericEncoder getEncoder(int encoderNum);
     
-//     @Override
-//     default void toLog(LogTable table) {
-//         table.put("MotorOutput", get());
-//         table.put("DrawCurrentAmps", getDrawCurrent());
-//         table.put("DeviceHealth", getHealth().toString());
-//         BreakerGenericEncoder.super.toLog(table.getSubtable("SelectedEncoder"));
-//     }
+    @Override
+    default void toLog(LogTable table) {
+        table.put("MotorOutput", get());
+        table.put("DrawCurrentAmps", getDrawCurrent());
+        table.put("DeviceHealth", getHealth().toString());
+    }
 
-//     // public static enum SmartMotorControlMode {
-//     //     DUTY_CYCLE,
-//     //     VOLTAGE,
-//     //     POSITION,
-//     //     VELOCITY,
-//     //     CURRENT,
-//     //     SMART_MOTION
-//     // }
+    // public static enum SmartMotorControlMode {
+    //     DUTY_CYCLE,
+    //     VOLTAGE,
+    //     POSITION,
+    //     VELOCITY,
+    //     CURRENT,
+    //     SMART_MOTION
+    // }
 
-//     // public static enum SmartMotorFeedforward {
-//     //     DUTY_CYCLE,
-//     //     VOLTAGE,
-//     //     POSITION,
-//     //     VELOCITY,
-//     //     CURRENT,
-//     //     SMART_MOTION
-//     // }
+    // public static enum SmartMotorFeedforward {
+    //     DUTY_CYCLE,
+    //     VOLTAGE,
+    //     POSITION,
+    //     VELOCITY,
+    //     CURRENT,
+    //     SMART_MOTION
+    // }
     
-// }
+}
--- src/main/java/frc/robot/BreakerLib/devices/motors/smart/BreakerTalonFX.java
+++ src/main/java/frc/robot/BreakerLib/devices/motors/smart/BreakerTalonFX.java
@@ -1,175 +1,179 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.BreakerLib.devices.motors.smart;
+package frc.robot.BreakerLib.devices.motors.smart;
 
-// import com.ctre.phoenix6.configs.FeedbackConfigs;
-// import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
-// import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
 
-// import frc.robot.BreakerLib.devices.encoders.BreakerGenericEncoder;
-// import frc.robot.BreakerLib.util.logging.advantagekit.LogTable;
-// import frc.robot.BreakerLib.util.test.selftest.DeviceHealth;
+import frc.robot.BreakerLib.devices.encoders.BreakerGenericEncoder;
+import frc.robot.BreakerLib.util.logging.advantagekit.LogTable;
+import frc.robot.BreakerLib.util.test.selftest.DeviceHealth;
 
-// /** Add your docs here. */
-// public class BreakerTalonFX extends TalonFX implements BreakerGenericSmartMotorController {
+/** Add your docs here. */
+public class BreakerTalonFX extends TalonFX implements BreakerGenericSmartMotorController {
+    private final VoltageOut voltageOut;
+    private final DutyCycleOut dutyCycleOut;
+    public BreakerTalonFX(PhoenixLicenceType licenceType, int deviceId, String canbus) {
+        super(deviceId, canbus);
+        voltageOut = new VoltageOut(0.0, licenceType.isProLicenced(), false);
+        dutyCycleOut = new DutyCycleOut(0.0, licenceType.isProLicenced(), false);
+    } 
 
-//     public BreakerTalonFX(int deviceId, String canbus) {
-//         super(deviceId, canbus);
-//         //TODO Auto-generated constructor stub
-//     } 
+    public BreakerTalonFX(PhoenixLicenceType licenceType, int deviceId) {
+        this(licenceType, deviceId, "");
+    }
 
-//     public BreakerTalonFX(int deviceId) {
-//         super(deviceId);
-//     }
-
-//     @Override
-//     public void set(double speed) {
-//         // TODO Auto-generated method stub
-//         super.set(speed);
-//     }
-
-//     @Override
-//     public void setVoltage(double volts) {
-//         // TODO Auto-generated method stub
-//         super.setVoltage(volts);
-//     }
-
-//     @Override
-//     public void runSelfTest() {
-//         // TODO Auto-generated method stub
+    @Override
+    public void runSelfTest() {
+        // TODO Auto-generated method stub
         
-//     }
+    }
 
-//     @Override
-//     public DeviceHealth getHealth() {
-       
-//         return null;
-//     }
+    @Override
+    public DeviceHealth getHealth() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-//     @Override
-//     public String getFaults() {
-//         // TODO Auto-generated method stub
-//         return null;
-//     }
+    @Override
+    public String getFaults() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-//     @Override
-//     public String getDeviceName() {
-//         // TODO Auto-generated method stub
-//         return null;
-//     }
+    @Override
+    public String getDeviceName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-//     @Override
-//     public boolean hasFault() {
-//         // TODO Auto-generated method stub
-//         return false;
-//     }
+    @Override
+    public boolean hasFault() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-//     @Override
-//     public void setDeviceName(String newName) {
-//         // TODO Auto-generated method stub
+    @Override
+    public void setDeviceName(String newName) {
+        // TODO Auto-generated method stub
         
-//     }
+    }
 
-//     @Override
-//     public void toLog(LogTable table) {
-//         // TODO Auto-generated method stub
+    @Override
+    public double getDrawCurrent() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setBrakeMode() {
         
-//     }
+    }
 
-//     @Override
-//     public void setBrakeMode() {
-//         // TODO Auto-generated method stub
-        
-//     }
+    @Override
+    public BreakerGenericEncoder getSelectedEncoder() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-//     @Override
-//     public double getDrawCurrent() {
-//         // TODO Auto-generated method stub
-//         return 0;
-//     }
+    @Override
+    public BreakerGenericEncoder getEncoder(int encoderNum) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-//     @Override
-//     public BreakerGenericEncoder getEncoder(int encoderNum) {
-//         switch (encoderNum) {
-//             case 0:
-//                 break;
-        
-//             default:
-//                 break;
-//         }
-//         return null;
-//     } 
+    @Override
+    public void set(double speed) {
+        setControl(dutyCycleOut.withOutput(speed));
+    }
 
-//     public static class BreakerTalonFXRotorEncoder implements BreakerGenericEncoder {
+    @Override
+    public void setVoltage(double volts) {
+        setControl(voltageOut.withOutput(volts));
+    }
 
-//         private BreakerTalonFX parent;
-//         public BreakerTalonFXRotorEncoder(BreakerTalonFX parent) {
-//             this.parent = parent;
-//         }
+    public static enum PhoenixLicenceType {
+        STANDARD(false),
+        PRO(true);
+ 
+        private final boolean isPro;
+        private PhoenixLicenceType(boolean isPro) {
+            this.isPro = isPro;
+        }
 
-//         @Override
-//         public void runSelfTest() {
-//             // TODO Auto-generated method stub
+        public boolean isProLicenced() {
+            return isPro;
+        }
+    }
+
+    public static class TalonFXRotorEncoder implements BreakerGenericEncoder {
+
+        @Override
+        public void runSelfTest() {
+            // TODO Auto-generated method stub
             
-//         }
+        }
 
-//         @Override
-//         public DeviceHealth getHealth() {
-//             // TODO Auto-generated method stub
-//             return null;
-//         }
+        @Override
+        public DeviceHealth getHealth() {
+            // TODO Auto-generated method stub
+            return null;
+        }
 
-//         @Override
-//         public String getFaults() {
-//             // TODO Auto-generated method stub
-//             return null;
-//         }
+        @Override
+        public String getFaults() {
+            // TODO Auto-generated method stub
+            return null;
+        }
 
-//         @Override
-//         public String getDeviceName() {
-//             // TODO Auto-generated method stub
-//             return null;
-//         }
+        @Override
+        public String getDeviceName() {
+            // TODO Auto-generated method stub
+            return null;
+        }
 
-//         @Override
-//         public boolean hasFault() {
-//             // TODO Auto-generated method stub
-//             return false;
-//         }
+        @Override
+        public boolean hasFault() {
+            // TODO Auto-generated method stub
+            return false;
+        }
 
-//         @Override
-//         public void setDeviceName(String newName) {
-//             // TODO Auto-generated method stub
+        @Override
+        public void setDeviceName(String newName) {
+            // TODO Auto-generated method stub
             
-//         }
+        }
 
-//         @Override
-//         public double getEncoderAbsolutePosition() {
-//             return parent.getConfigurator().apply(new FeedbackConfigs().FeedbackSensorSource);
-//         }
+        @Override
+        public double getEncoderAbsolutePosition() {
+            // TODO Auto-generated method stub
+            return 0;
+        }
 
-//         @Override
-//         public double getEncoderRelativePosition() {
-//             // TODO Auto-generated method stub
-//             return 0;
-//         }
+        @Override
+        public double getEncoderRelativePosition() {
+            // TODO Auto-generated method stub
+            return 0;
+        }
 
-//         @Override
-//         public double getEncoderVelocity() {
-//             // TODO Auto-generated method stub
-//             return 0;
-//         }
+        @Override
+        public double getEncoderVelocity() {
+            // TODO Auto-generated method stub
+            return 0;
+        }
 
-//         @Override
-//         public double setEncoderPosition(double newPosition) {
-//             // TODO Auto-generated method stub
-//             return 0;
-//         }
-        
-//     }
+        @Override
+        public double setEncoderPosition(double newPosition) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
 
+    }
     
 
-// }
+}
